f78d09a9cd06eba9d5aeca3022c6c178a95f32b9
Mobin Daneshmand <mb102.dev@gmail.com>
2022-08-29 18:07:32 +0430

    to do lsit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,5 @@
-import { Component } from '@angular/core';
-
+import {Component, ElementRef, ViewChild,OnInit} from '@angular/core';
+import {FormControl, FormGroup, Validators} from "@angular/forms";
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
@@ -7,4 +7,41 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'TODO';
+  inputForm:FormGroup;
+  @ViewChild("task", {static: true}) task: ElementRef;
+  list: any[] = [];
+  max:boolean=false;
+
+  public addTask(item: string) {
+    this.list.push({id:this.list.length,name:item})
+    console.log('task',this.task)
+    this.task.nativeElement.value = null;
+    console.log(this.inputForm)
+  }
+
+  public onRemove(id: number) {
+    this.list = this.list.filter(item => item.id !== id)
+  }
+  public disabledButton(): boolean  {
+    if (this.task.nativeElement.value == '') {
+      return true
+    }
+    return false
+  };
+  public onEdit(id: number) {
+    let tittle = this.list[id].name;
+    let result=prompt("Edit Task",tittle);
+    if (result !== null && result !== "") {
+      this.list[id].name = result;
+    }
+  }
+
+
+
+  ngOnInit():void{
+    this.inputForm=new FormGroup({
+      'task': new FormControl(null,[Validators.required,Validators.maxLength(60)])
+    })
+  }
+
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,37 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-
+import { AddTaskComponent } from './add-task/add-task.component';
+import {FlexModule} from "@angular/flex-layout";
+import {NzCardModule} from "ng-zorro-antd/card";
+import {NzListModule} from "ng-zorro-antd/list";
+import {NzTypographyModule} from "ng-zorro-antd/typography";
+import { TaskListComponent } from './task-list/task-list.component';
+import {NzFormModule} from "ng-zorro-antd/form";
+import {NzInputModule} from "ng-zorro-antd/input";
+import {FormsModule} from "@angular/forms";
+import {NzWaveModule} from "ng-zorro-antd/core/wave";
+import {NzButtonModule} from "ng-zorro-antd/button";
+import {ReactiveFormsModule} from "@angular/forms";
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    AddTaskComponent,
+    TaskListComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FlexModule,
+    NzCardModule,
+    NzListModule,
+    NzTypographyModule,
+    NzFormModule,
+    NzInputModule,
+    FormsModule,
+    NzWaveModule,
+    NzButtonModule,
+    ReactiveFormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
